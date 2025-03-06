26affb23fa5060ebb8ed17017e6c2c03fbdf6c68
md tausif <you@example.com>
2025-03-07 02:04:05 +0530

    Update CORS origin to use deployed frontend URL

--- backend/index.js
+++ backend/index.js
@@ -16,11 +16,10 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
-const corsOption={
-    origin:'http://localhost:3000',
-    credentials:true
-};
-app.use(cors(corsOption)); 
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'https://chat-app-ieu9.onrender.com/',
+    credentials: true
+})); 
 
 
 // routes
